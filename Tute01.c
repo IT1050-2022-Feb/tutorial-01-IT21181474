7fd5fdec2d5995a6cc97fd0765ea9ae0c10d3d47
Yashini <99383400+IT21181474@users.noreply.github.com>
2022-02-13 10:56:10 +0000

    I have completed four questions.

--- Tute01.c
+++ Tute01.c
@@ -8,7 +8,7 @@ int main() {
   int subject1,subject2;
   float average=0; 
   printf("Enter subjects' marks : ");
-  scanf("%d %d",&subject1,&subject2); //get user inputs
+  scanf("%d %d",&subject1,&subject2); //get user input
   average=(subject1+subject2)/2.0;  //calculate average
   printf("Average is %f",average);
   return 0;
